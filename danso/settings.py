ae3f99a8cf08e535f09189d1fad5759f910b0735
Yuchan Han <serltretu24@gmail.com>
2025-06-19 10:17:09 +0900

    Add `CSRF_TRUSTED_ORIGINS` configuration to `settings.py`.

--- danso/settings.py
+++ danso/settings.py
@@ -130,6 +130,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://danso-api.thnos.app",
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
